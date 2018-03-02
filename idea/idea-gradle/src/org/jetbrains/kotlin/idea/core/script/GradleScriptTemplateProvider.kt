384800ec3be476de786c7fa7f62f693997408501
Natalia Selezneva <natalia.selezneva@jetbrains.com>
2018-03-02 14:30:44 +0300

    Don't highlight scripts if resolver returns Failure. Note that we don't highlight script when ScriptDependenciesFileAttribute is not set.

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/core/script/GradleScriptTemplateProvider.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/core/script/GradleScriptTemplateProvider.kt
@@ -87,8 +87,6 @@ class GradleScriptDefinitionsContributor(private val project: Project): ScriptDe
         return loadDefinitions()
     }
 
-    override fun isError() = failedToLoad.get()
-
     // NOTE: control flow here depends on suppressing exceptions from loadGradleTemplates calls
     // TODO: possibly combine exceptions from every loadGradleTemplates call, be mindful of KT-19276
     private fun loadDefinitions(): List<KotlinScriptDefinition> {
