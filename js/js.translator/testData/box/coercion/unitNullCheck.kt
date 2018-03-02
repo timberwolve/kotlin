a3e5eb5e2334018404e7476d2de459e83ea9094a
Anton Bannykh <Anton.Bannykh@jetbrains.com>
2018-03-02 18:08:55 +0300

    Add a test for null materialization bugs

--- js/js.translator/testData/box/coercion/unitNullCheck.kt
+++ js/js.translator/testData/box/coercion/unitNullCheck.kt
@@ -1,18 +1,33 @@
 // EXPECTED_REACHABLE_NODES: 1132
 var log = ""
 
+fun log(msg: String) {
+    log += "$msg;"
+}
+
+lateinit var unit: Unit
+
 fun box(): String {
-    foo() ?: bar()
-    if (foo() == null) bar()
+    log("!!")!!
 
-    if (log != "foo;foo;") return "fail: $log"
+    unit = log("lateinit")
+    log(unit.toString())
+
+    log("elvis-left") ?: log("elvis-right")
+
+    if (log("if") == null) log("then")
+
+    when (null) {
+        log("when-pattern") -> log("when-body-1")
+    }
+
+    when (log("when-subject")) {
+        null -> log("when-body-2")
+    }
+
+    log += log("?.")?.toString()
+
+    if (log != "!!;lateinit;kotlin.Unit;elvis-left;if;when-pattern;when-subject;?.;") return "fail: $log"
+
     return "OK"
-}
-
-fun foo() {
-    log += "foo;"
-}
-
-fun bar() {
-    log += "bar;"
 }
